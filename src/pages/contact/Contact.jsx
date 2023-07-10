97e716a2f6899f658cc09a97dea573933719ebc1
Shehab Waleed <shehabwaleedd@gmail.com>
2023-07-10 06:27:54 +0400

    created contact page.

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,8 +3,25 @@ import './Contact.css'
 
 const Contact = () => {
     return (
-        <section>
-
+        <section className='contact'>
+            <div className="contact__container container">
+                <div className="contact__left">
+                    <div className="left__upper">
+                        <h1>GENERAL ENQUIRIES & COMMERCIAL BOOKINGS</h1>
+                        <span><a href="mailto: info@jinghuima.com">info@jinghuima.com</a></span>
+                    </div>
+                </div>
+                <div className="contact__right">
+                    <div className="left__lower">
+                        <h1>IMAGE LICENSING REQUESTS</h1>
+                        <div className="lower__content">
+                            <h1>JIN ARCHIVE</h1>
+                            <span><a href="mailto: info@jinghuima.com">contact@jinghuima.com</a></span>
+                            <p>+13073399113</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
