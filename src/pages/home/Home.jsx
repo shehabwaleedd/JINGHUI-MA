7f6196018a690ce69a8f665654058c5639371a7d
Shehab Waleed <shehabwaleedd@gmail.com>
2023-07-30 18:05:24 +0400

    minor improvments on performance.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import './Home.css';
 import Data from './Data';
-import { AnimatePresence } from 'framer-motion';
-import { motion } from 'framer-motion';
 import { useEffect } from 'react';
 import { useSwipeable } from 'react-swipeable';
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 import MobileHomeResponsive from './homeResponsive/mobileHomeResponsive/MobileHomeResponsive';
+import DesktopHome from './homeResponsive/desktopHomeResponsive/DesktopHome';
 
 const Home = ({ isMobile }) => {
     const [selectedImage, setSelectedImage] = useState(null);
@@ -57,61 +54,9 @@ const Home = ({ isMobile }) => {
     return (
         <>
             {isMobile ? (
-                <MobileHomeResponsive
-                    openPreview={openPreview}
-                    selectedImage={selectedImage}
-                    setSelectedImage={setSelectedImage}
-                    currentIndex={currentIndex}
-                    setCurrentIndex={setCurrentIndex}
-                    closePreview={closePreview}
-                    handleModalSwipe={handleModalSwipe}
-                    handleContainerSwipe={handleContainerSwipe}
-                    handleSwipeLeft={handleSwipeLeft}
-                    handleSwipeRight={handleSwipeRight}
-
-                />
-
+                <MobileHomeResponsive openPreview={openPreview} selectedImage={selectedImage} setSelectedImage={setSelectedImage} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} closePreview={closePreview} handleModalSwipe={handleModalSwipe} handleContainerSwipe={handleContainerSwipe} handleSwipeLeft={handleSwipeLeft} handleSwipeRight={handleSwipeRight}/>
             ) : (
-                <motion.main className='home' initial={{ opacity: 0, y: 150 }} animate={{ opacity: 1, y: 0, transition: { delay: 0.5, staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] } }} exit={{ opacity: 0, y: 150, transition: { delay: 0.3, velocity: 2, staggerChildren: 1.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }}>
-                    <div className='home__container container'>
-                        <div className='home__grid'>
-                            {Data.map(({ img, id, img_300px, img_600px, img_900px }, index) => (
-                                <div key={id} className='grid__item' onClick={() => openPreview(img, index)}>
-                                    <LazyLoadImage
-                                        src={img}
-                                        alt='sass'
-                                        effect='blur'
-                                        srcSet={[
-                                            `${img_300px}?w=300&format=webp 300w`,
-                                        ]}
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                    <AnimatePresence>
-                        {selectedImage && (
-                            <motion.div className='preview__modal' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }} exit={{ opacity: 0 }}
-                                {...handleModalSwipe}>
-                                <div className='preview__close'>
-                                    <button onClick={closePreview}>X</button>
-                                </div>
-
-                                <motion.div className='preview__content'>
-                                    <div className='preview__buttons'>
-                                        <button className='button__prev' onClick={handleSwipeRight} disabled={currentIndex === 0}>
-                                            <IoIosArrowBack />
-                                        </button>
-                                        <button className='button__next' onClick={handleSwipeLeft} disabled={currentIndex === Data.length - 1}>
-                                            <IoIosArrowForward />
-                                        </button>
-                                    </div>
-                                    <motion.img initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }} exit={{ opacity: 0 }} src={selectedImage} alt='Preview' />
-                                </motion.div>
-                            </motion.div>
-                        )}
-                    </AnimatePresence>
-                </motion.main>
+                <DesktopHome openPreview={openPreview} selectedImage={selectedImage} setSelectedImage={setSelectedImage} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} closePreview={closePreview} handleModalSwipe={handleModalSwipe} handleContainerSwipe={handleContainerSwipe} handleSwipeLeft={handleSwipeLeft} handleSwipeRight={handleSwipeRight} />
             )}
         </>
     );
---
+++ src/pages/home/homeResponsive/desktopHomeResponsive/DesktopHome.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './DesktopHome.css'
+import Data from '../../Data'
+import { motion, AnimatePresence } from 'framer-motion'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import { IoIosArrowBack } from 'react-icons/io'
+import { IoIosArrowForward } from 'react-icons/io'
+
+const DesktopHome = ({ selectedImage, currentIndex, handleSwipeLeft, handleSwipeRight, handleModalSwipe, openPreview, closePreview}) => {
+    return (
+        <motion.main className='home' initial={{ opacity: 0, y: 150 }} animate={{ opacity: 1, y: 0, transition: { delay: 0.5, staggerChildren: 3.5, duration: 0.7, ease: [0.42, 0, 0.58, 1] } }} exit={{ opacity: 0, y: 150, transition: { delay: 0.3, velocity: 2, staggerChildren: 1.5, duration: 1, ease: [0.42, 0, 0.58, 1] } }}>
+            <div className='home__container container'>
+                <div className='home__grid'>
+                    {Data.map(({ img, id, img_300px, img_600px, img_900px }, index) => (
+                        <div key={id} className='grid__item' onClick={() => openPreview(img, index)}>
+                            <LazyLoadImage src={img} alt='sass' effect='blur' srcSet={[`${img_300px}?w=300&format=webp 300w`]}/>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <AnimatePresence>
+                {selectedImage && (
+                    <motion.div className='preview__modal' initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }} exit={{ opacity: 0 }}
+                        {...handleModalSwipe}>
+                        <div className='preview__close'>
+                            <button onClick={closePreview}>X</button>
+                        </div>
+                        <motion.div className='preview__content'>
+                            <div className='preview__buttons'>
+                                <button className='button__prev' onClick={handleSwipeRight} disabled={currentIndex === 0}>
+                                    <IoIosArrowBack />
+                                </button>
+                                <button className='button__next' onClick={handleSwipeLeft} disabled={currentIndex === Data.length - 1}>
+                                    <IoIosArrowForward />
+                                </button>
+                            </div>
+                            <motion.img initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }} exit={{ opacity: 0 }} src={selectedImage} alt='Preview' />
+                        </motion.div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </motion.main>
+    )
+}
+
+export default DesktopHome
